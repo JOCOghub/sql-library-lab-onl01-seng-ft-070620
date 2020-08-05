b6a9bbc51d4ad1ca215babf45cc296ec32a7b92e
Joseph cohen <jojocohen57@gmail.com>
2020-08-05 01:52:58 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,9 +1,10 @@
-CREATE TABLE Series(
-id INTEGER PRIMARY KEY,
-title, TEXT 
-author_id INTEGER,
-subgenre_id INTEGER
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
 );
+
 CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
